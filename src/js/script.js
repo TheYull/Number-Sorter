a103038ce46f73f37beb5041af9aa5e4d1404f19
Yuliia <the.smile.too@gmail.com>
2024-04-01 23:42:37 +0300

    update selectionSort

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
 
-  const sortedValues = bubbleSort(inputValues);
+  const sortedValues = selectionSort(inputValues);
 
   updateUI(sortedValues);
 };
@@ -33,6 +33,11 @@ const bubbleSort = (array) => {
   return array;
 };
 
-const selectionSort = (array) => {};
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {}
+  }
+};
 
 sortButton.addEventListener("click", sortInputArray);
